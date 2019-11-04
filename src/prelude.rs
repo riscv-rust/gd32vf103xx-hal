761814e4ce4df3ffbc67d56f1c2983e847717de9
Vadim Kaushan <admin@disasm.info>
2019-11-04 17:23:36 +0300

    Refactor prelude

--- src/prelude.rs
+++ src/prelude.rs
@@ -1,5 +1,5 @@
-pub use crate::gpio::GpioExt as _gd32_hal_gpio_GpioExt;
-pub use crate::hal::digital::v2::StatefulOutputPin as _embedded_hal_digital_StatefulOutputPin;
-pub use crate::hal::digital::v2::ToggleableOutputPin as _embedded_hal_digital_ToggleableOutputPin;
 pub use crate::hal::prelude::*;
-pub use crate::rcu::RcuExt as _gd32_hal_rcu_RcuExt;
+
+pub use crate::gpio::GpioExt as _gd32vf103xx_hal_gpio_GpioExt;
+pub use crate::rcu::RcuExt as _gd32vf103xx_hal_rcu_RcuExt;
+pub use crate::time::U32Ext as _gd32vf103xx_hal_time_U32Ext;
