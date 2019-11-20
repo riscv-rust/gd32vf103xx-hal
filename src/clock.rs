fa1f785a7a0498a6faaddfd116ce4014afda6639
Vadim Kaushan <admin@disasm.info>
2019-11-21 00:57:46 +0300

    Dummy clock settings

--- src/clock.rs
+++ src/clock.rs
@@ -3,11 +3,14 @@ use crate::time::Hertz;
 pub struct Clocks;
 
 impl Clocks {
+    fn base(&self) -> Hertz {
+        Hertz(8_000_000)
+    }
     pub fn pclk1(&self) -> Hertz {
-        Hertz(54_000_000)
+        Hertz(self.base().0 / 2)
     }
 
     pub fn pclk2(&self) -> Hertz {
-        Hertz(108_000_000)
+        self.base()
     }
 }
