e5be517615174489732edd3b35327e9b0db9c9c6
Stéphane Clérambault <stephane@clerambault.fr>
2020-11-12 09:11:15 +0100

    Separate Inserted and Regural Channels OneShot.
    The STM32 ADC implementation doesn't support the difference between Regular and
    Inserted channels.
    
    For the moment only the OneShot is implemented. The intermediates
    AdcRegularOneShot and AdcInsertedOneShot struct are use to configure the
    channel use by the ADC.
    
    ContinuousMode or ScanMode will be implemented later.

--- src/adc.rs
+++ src/adc.rs
@@ -144,64 +144,111 @@ pub enum ConversionMode {
 /// ADC configuration
 pub struct Adc<ADC> {
     adc: ADC,
-    sample_time: SampleTime,
-    align: Align,
     clocks: Clocks,
 }
 
+pub struct AdcRegularOneShot<ADC> {
+    adc: ADC,
+    sample_time: SampleTime,
+    align: Align,
+}
 
+pub struct AdcInsertedOneShot<ADC> {
+    adc: ADC,
+    sample_time: SampleTime,
+    offset: u16,
+    align: Align,
+}
+
+pub trait AdcRegularChannel {
+    fn set_channel_sequence(&mut self, channels: &[u8]);
+    fn set_channel_sample_time(&mut self, channel: u8, sample_time: SampleTime);
+    fn oneshot_setup(&mut self, align: Align);
+    fn oneshot_convert(&mut self) -> u16;
+    fn power_down(&mut self);
+}
+
+pub trait AdcInsertedChannel {
+    fn set_channel_sequence(&mut self, channels: &[u8]);
+    fn set_channel_offset(&mut self, channel: u8, offset: u16);
+    fn set_channel_sample_time(&mut self, channel: u8, sample_time: SampleTime);
+    fn oneshot_setup(&mut self, align: Align);
+    fn oneshot_convert(&mut self) -> i16;
+    fn power_down(&mut self);
+}
 
 macro_rules! adc_hal {
     ($(
         $ADC:ident: ($adc:ident),
     )+) => {
         $(
-
             impl Adc<$ADC> {
                 /// Init a new Adc
                 ///
                 /// Sets all configurable parameters to one-shot defaults,
                 /// performs a boot-time calibration.
                 pub fn new(rcu: &mut Rcu, adc: $ADC) -> Self {
-                    let mut s = Self {
-                        adc: adc,
-                        sample_time: SampleTime::default(),
-                        align: Align::default(),
-                        clocks: rcu.clocks,
-                    };
-
                     $ADC::enable(rcu);
                     $ADC::reset(rcu);
 
-                    s.power_up();
-                    s.calibrate();
-                    s
-                }
-
-
-                /// Set ADC sampling time
-                ///
-                /// Options can be found in [SampleTime](crate::adc::SampleTime).
-                pub fn set_sample_time(&mut self, t_samp: SampleTime) {
-                    self.sample_time = t_samp;
-                }
-
-                /// Set the Adc result alignment
-                ///
-                /// Options can be found in [Align](crate::adc::Align).
-                pub fn set_align(&mut self, align: Align) {
-                    self.align = align;
-                }
-
-                /// Returns the largest possible sample value for the current settings
-                pub fn max_sample(&self) -> u16 {
-                    match self.align {
-                        Align::Left => u16::max_value(),
-                        Align::Right => (1 << 12) - 1,
+                    Self {
+                        adc: adc,
+                        clocks: rcu.clocks,
                     }
                 }
 
-                fn power_up(&mut self) {
+                pub fn inserted_oneshot(self) -> AdcInsertedOneShot<Self> {
+                    AdcInsertedOneShot {
+                        adc: self,
+                        sample_time: SampleTime::default(),
+                        align: Align::default(),
+                        offset: 0,
+                    }
+                }
+
+                pub fn inserted_oneshot_setup(&mut self, align: Align) {
+                    /* Configure in Free Sync mode */
+                    self.adc.ctl0.write(|w| unsafe { w.syncm().bits(0) });
+
+                    self.adc.ctl1.write(|w| unsafe { w
+                        /* Configure software trigger */
+                        .etsic().bits(0b111)
+                        /* Enable extern trigger */
+                        .eteic().set_bit()
+                        /* Set Alignement */
+                        .dal().bit(align.into())
+                    });
+
+                    self.power_up();
+                    self.calibrate();
+                }
+
+                pub fn regular_oneshot(self) -> AdcRegularOneShot<Self> {
+                    AdcRegularOneShot {
+                        adc: self,
+                        sample_time: SampleTime::default(),
+                        align: Align::default(),
+                    }
+                }
+
+                pub fn regular_oneshot_setup(&mut self, align: Align) {
+                    /* Configure in Free Sync mode */
+                    self.adc.ctl0.write(|w| unsafe { w.syncm().bits(0) });
+
+                    self.adc.ctl1.write(|w| unsafe { w
+                        /* Configure software trigger */
+                        .etsrc().bits(0b111)
+                        /* Enable extern trigger */
+                        .eterc().set_bit()
+                        /* Set Alignement */
+                        .dal().bit(align.into())
+                    });
+
+                    self.power_up();
+                    self.calibrate();
+                }
+
+                pub fn power_up(&mut self) {
                     /* Up and wait ADC is stabilized. */
                     self.adc.ctl1.modify(|_, w| w.adcon().set_bit());
 
@@ -214,7 +261,7 @@ macro_rules! adc_hal {
                     delay.delay_us(14_000_000 / clocks.adc().0);
                 }
 
-                fn power_down(&mut self) {
+                pub fn power_down(&mut self) {
                     self.adc.ctl1.modify(|_, w| w.adcon().clear_bit());
                 }
 
@@ -224,20 +271,6 @@ macro_rules! adc_hal {
 
                     self.adc.ctl1.modify(|_, w| { w.clb().set_bit() });
                     while self.adc.ctl1.read().clb().bit_is_set() {}
-                }
-
-                fn setup_oneshot(&mut self) {
-                    /* Configure in Free Sync mode */
-                    self.adc.ctl0.write(|w| unsafe { w.syncm().bits(0) });
-
-                    self.adc.ctl1.write(|w| unsafe { w
-                        /* Configure software trigger */
-                        .etsrc().bits(0b111)
-                        /* Enable extern trigger */
-                        .eterc().set_bit()
-                        /* MSB Alignement */
-                        .dal().set_bit()
-                    });
                 }
 
                 pub fn set_channel_sample_time(&mut self, chan: u8, sample_time: SampleTime) {
@@ -266,9 +299,7 @@ macro_rules! adc_hal {
                     }
                 }
 
-                pub fn set_conversion_mode(&mut self, mode: ConversionMode) {}
-
-                pub fn set_regular_sequence (&mut self, channels: &[u8]) {
+                pub fn set_regular_sequence(&mut self, channels: &[u8]) {
                     let len = channels.len();
                     let bits = channels.iter().take(6).enumerate().fold(0u32, |s, (i, c)|
                         s | ((*c as u32) << (i * 5))
@@ -297,22 +328,30 @@ macro_rules! adc_hal {
                     self.adc.rsq0.modify(|_, w| unsafe { w.rl().bits((len-1) as u8) });
                 }
 
-                fn set_continuous_mode(&mut self, continuous: bool) {}
+                pub fn set_inserted_sequence(&mut self, channels: &[u8]) {
+                    let len = channels.len();
 
-                fn set_scan_mode(&mut self) {}
-
-                fn set_discontinuous_mode(&mut self, channels_count: Option<u8>) {}
-
-                fn convert(&mut self, chan: u8) -> u16 {
-                    self.adc.ctl1.modify(|_, w| w
-                        /* MSB Alignement */
-                        .dal().set_bit()
+                    let bits = channels.iter().enumerate().fold(0u32, |s, (i, c)|
+                        s | ((*c as u32) << (4-len+i)*5)
                     );
 
-                    self.adc.ctl1.modify(|_, w| w
-                        /* trigger a new sample */
-                        .swrcst().set_bit()
-                    );
+                    self.adc.isq.write(|w| unsafe { w.bits(bits) });
+                    self.adc.isq.modify(|_, w| unsafe { w.il().bits((len-1) as u8) });
+                }
+
+                pub fn set_inserted_channel_offset(&mut self, chan: u8, offset: u16) {
+                    match chan {
+                        0 => self.adc.ioff0.write(|w| unsafe { w.ioff().bits(offset) } ),
+                        1 => self.adc.ioff1.write(|w| unsafe { w.ioff().bits(offset) } ),
+                        2 => self.adc.ioff2.write(|w| unsafe { w.ioff().bits(offset) } ),
+                        3 => self.adc.ioff3.write(|w| unsafe { w.ioff().bits(offset) } ),
+                        _ => {}
+                    }
+                }
+
+                pub fn regular_oneshot_convert(&mut self) -> u16 {
+                    /* trigger a new sample */
+                    self.adc.ctl1.modify(|_, w| w.swrcst().set_bit());
 
                     while self.adc.stat.read().eoc().bit_is_clear() {}
 
@@ -321,20 +360,97 @@ macro_rules! adc_hal {
 
                     rdata
                 }
+
+                pub fn inserted_oneshot_convert(&mut self) -> i16 {
+                    /* trigger a new sample */
+                    self.adc.ctl1.modify(|_, w| w.swicst().set_bit());
+
+                    while self.adc.stat.read().eoc().bit_is_clear() {}
+                    while self.adc.stat.read().eoic().bit_is_clear() {}
+
+                    let idata = self.adc.idata0.read().idatan().bits();
+                    self.adc.stat.modify(|_, w| w
+                        .eoc().clear_bit()
+                        .eoic().clear_bit()
+                    );
+
+                    idata as i16
+                }
             }
 
-            impl<WORD, PIN> OneShot<$ADC, WORD, PIN> for Adc<$ADC>
+            impl AdcRegularChannel for Adc<$ADC> {
+                fn set_channel_sequence(&mut self, channels: &[u8]) {
+                    self.set_regular_sequence(channels);
+                }
+
+                fn set_channel_sample_time(&mut self, channel: u8, sample_time: SampleTime) {
+                    self.set_channel_sample_time(channel, sample_time);
+                }
+
+                fn oneshot_setup(&mut self, align: Align) {
+                    self.regular_oneshot_setup(align)
+                }
+
+                fn oneshot_convert(&mut self) -> u16 {
+                    self.regular_oneshot_convert()
+                }
+
+                fn power_down(&mut self) {
+                    self.power_down();
+                }
+            }
+
+            impl AdcInsertedChannel for Adc<$ADC> {
+                fn set_channel_sequence(&mut self, channels: &[u8]) {
+                    self.set_inserted_sequence(channels);
+                }
+
+                fn set_channel_sample_time(&mut self, channel: u8, sample_time: SampleTime) {
+                    self.set_channel_sample_time(channel, sample_time);
+                }
+
+                fn set_channel_offset(&mut self, channel: u8, offset: u16) {
+                    self.set_inserted_channel_offset(channel, offset);
+                }
+
+                fn oneshot_setup(&mut self, align: Align) {
+                    self.inserted_oneshot_setup(align)
+                }
+
+                fn oneshot_convert(&mut self) -> i16 {
+                    self.inserted_oneshot_convert()
+                }
+
+                fn power_down(&mut self) {
+                    self.power_down();
+                }
+            }
+
+            impl<WORD, PIN> OneShot<$ADC, WORD, PIN> for AdcRegularOneShot<Adc<$ADC>>
             where
                 WORD: From<u16>,
                 PIN: Channel<$ADC, ID = u8>,
-                {
-                    type Error = ();
+            {
+                type Error = ();
 
-                    fn read(&mut self, _pin: &mut PIN) -> nb::Result<WORD, Self::Error> {
-                        let res = self.convert(PIN::channel());
-                        Ok(res.into())
-                    }
+                fn read(&mut self, _pin: &mut PIN) -> nb::Result<WORD, Self::Error> {
+                    let res = self.convert(PIN::channel());
+                    Ok(res.into())
                 }
+            }
+
+            impl<WORD, PIN> OneShot<$ADC, WORD, PIN> for AdcInsertedOneShot<Adc<$ADC>>
+            where
+                WORD: From<i16>,
+                PIN: Channel<$ADC, ID = u8>,
+            {
+                type Error = ();
+
+                fn read(&mut self, _pin: &mut PIN) -> nb::Result<WORD, Self::Error> {
+                    let res = self.convert(PIN::channel());
+                    Ok(res.into())
+                }
+            }
         )+
     }
 }
@@ -342,3 +458,94 @@ macro_rules! adc_hal {
 adc_hal! {
     ADC0: (adc0),
 }
+
+macro_rules! adc_common_oneshot {
+    ($ONESHOT:ident, $ch_type:ident, $result:ty) => {
+        impl<T: $ch_type > $ONESHOT<T>
+        {
+            pub fn convert(&mut self, channel: u8) -> $result {
+                self.prepare(channel);
+                self.adc.oneshot_convert()
+            }
+
+            pub fn configure(mut self) -> Self {
+                self.adc.oneshot_setup(self.align);
+                self
+            }
+
+            pub fn release(mut self) -> T {
+                self.adc.power_down();
+                self.adc
+            }
+
+            /// Set ADC sampling time
+            ///
+            /// Options can be found in [SampleTime](crate::adc::SampleTime).
+            pub fn set_sample_time(mut self, t_samp: SampleTime) -> Self {
+                self.sample_time = t_samp;
+                self
+            }
+
+            /// Set the Adc result alignment
+            ///
+            /// Options can be found in [Align](crate::adc::Align).
+            pub fn set_align(mut self, align: Align) -> Self {
+                self.align = align;
+                self
+            }
+        }
+    }
+}
+
+macro_rules! adc_oneshot {
+    (Regular  => $ch_type:ident) => {
+        impl<T: $ch_type > AdcRegularOneShot<T> {
+            fn prepare(&mut self, channel: u8) {
+                self.adc.set_channel_sequence( &[channel]);
+                self.adc.set_channel_sample_time(channel, self.sample_time);
+            }
+
+            /// Returns the largest possible sample value for the current settings
+            pub fn max_sample(&self) -> u16 {
+                match self.align {
+                    Align::Left => u16::MAX,
+                    Align::Right => (1 << 12) - 1,
+                }
+            }
+        }
+
+        adc_common_oneshot!(AdcRegularOneShot, $ch_type, u16);
+    };
+
+    (Inserted => $ch_type:ident) => {
+        impl<T: $ch_type > AdcInsertedOneShot<T> {
+            fn prepare(&mut self, channel: u8) {
+                self.adc.set_channel_sequence( &[channel]);
+                self.adc.set_channel_offset(channel, self.offset);
+                self.adc.set_channel_sample_time(channel, self.sample_time);
+            }
+            /// Set the inserted offset
+            ///
+            /// Inserted Channels might have a 12-bits offset substracted
+            /// on the raw value (before alignement).
+            pub fn set_offset(mut self, offset: u16) -> Self {
+                self.offset = offset;
+                self
+            }
+
+            /// Returns the largest possible sample value for the current settings
+            pub fn max_sample(&self) -> i16 {
+                match self.align {
+                    Align::Left => i16::MAX - self.offset as i16,
+                    Align::Right => (1 << 12) - 1 - self.offset as i16,
+                }
+            }
+        }
+
+        adc_common_oneshot!(AdcInsertedOneShot, $ch_type, i16);
+    }
+}
+
+adc_oneshot!(Regular => AdcRegularChannel);
+adc_oneshot!(Inserted => AdcInsertedChannel);
+
